f116d9db840c1b7c326c99c1f8308191e194abcd
Solo Dev <solodev001@gmail.com>
2023-06-24 23:44:12 +0100

    add first code files

---
+++ index.js
@@ -0,0 +1,25 @@
+let myLeads = []
+
+const inputEl = document.querySelector('#input-el')
+const inputBtn = document.querySelector('#input-btn')
+const ulEl = document.querySelector('#ul-el')
+
+inputBtn.addEventListener('click', function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ''
+    for (i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+        <a href="${myLeads[i]}" target="_blank">
+        ${myLeads[i]}
+        </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
